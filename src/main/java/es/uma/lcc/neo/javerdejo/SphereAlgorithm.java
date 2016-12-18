e03eba9afadbe403d5b6339321bf37fd249aacf8
Javier Arellano Verdejo <javier_arellano_verdejo@hotmail.com>
2016-12-18 18:29:32 +0100

    Version 1

---
+++ src/main/java/es/uma/lcc/neo/javerdejo/SphereAlgorithm.java
@@ -0,0 +1,55 @@
+package es.uma.lcc.neo.javerdejo;
+
+import es.uma.lcc.neo.javerdejo.SphereFunction.SphereProblem;
+import org.uma.jmetal.algorithm.Algorithm;
+import org.uma.jmetal.algorithm.singleobjective.geneticalgorithm.GeneticAlgorithmBuilder;
+import org.uma.jmetal.operator.CrossoverOperator;
+import org.uma.jmetal.operator.MutationOperator;
+import org.uma.jmetal.operator.SelectionOperator;
+import org.uma.jmetal.operator.impl.crossover.SBXCrossover;
+import org.uma.jmetal.operator.impl.mutation.PolynomialMutation;
+import org.uma.jmetal.operator.impl.selection.BinaryTournamentSelection;
+import org.uma.jmetal.problem.DoubleProblem;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.util.AlgorithmRunner;
+import org.uma.jmetal.util.JMetalException;
+import org.uma.jmetal.util.JMetalLogger;
+import org.uma.jmetal.util.comparator.RankingComparator;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.uma.jmetal.runner.AbstractAlgorithmRunner.printFinalSolutionSet;
+
+public class SphereAlgorithm {
+    public static void main(String[] args) throws JMetalException, FileNotFoundException {
+
+        Algorithm<DoubleSolution> algorithm;
+        DoubleProblem problem = new SphereProblem(5);
+
+        CrossoverOperator<DoubleSolution> crossover = new SBXCrossover(0.9, 20.0) ;
+        MutationOperator<DoubleSolution> mutation  = new PolynomialMutation(1.0 / problem.getNumberOfVariables(), 20.0) ;
+        SelectionOperator<List<DoubleSolution>, DoubleSolution> selection = new BinaryTournamentSelection<DoubleSolution>(new RankingComparator<DoubleSolution>());
+
+        algorithm = new GeneticAlgorithmBuilder<DoubleSolution>(problem, crossover, mutation)
+                .setSelectionOperator(selection)
+                .setMaxEvaluations(25000)
+                .setPopulationSize(100)
+                .setVariant(GeneticAlgorithmBuilder.GeneticAlgorithmVariant.STEADY_STATE)
+                .build();
+
+        AlgorithmRunner algorithmRunner = new AlgorithmRunner.Executor(algorithm)
+                .execute();
+
+        DoubleSolution solution = algorithm.getResult();
+        List<DoubleSolution> population = new ArrayList<DoubleSolution>(1);
+        population.add(solution);
+
+        long computingTime = algorithmRunner.getComputingTime();
+        JMetalLogger.logger.info("Total execution time: " + computingTime + "ms");
+
+        printFinalSolutionSet(population);
+
+    }
+}
---
+++ src/main/java/es/uma/lcc/neo/javerdejo/SphereFunction/SphereProblem.java
@@ -0,0 +1,48 @@
+package es.uma.lcc.neo.javerdejo.SphereFunction;
+
+import org.uma.jmetal.problem.impl.AbstractDoubleProblem;
+import org.uma.jmetal.solution.DoubleSolution;
+import org.uma.jmetal.solution.impl.DefaultDoubleSolution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SphereProblem extends AbstractDoubleProblem {
+
+    public SphereProblem(Integer numberOfVariables) {
+        setNumberOfVariables(numberOfVariables);
+        setNumberOfObjectives(1);
+        setName("Sphere Problem");
+
+        List<Double> lowerLimit = new ArrayList<Double>();
+        List<Double> upperLimit = new ArrayList<Double>();
+
+        for (int i = 0; i < getNumberOfVariables(); i++) {
+            lowerLimit.add(-10.0);
+            upperLimit.add(10.0);
+        }
+
+        setLowerLimit(lowerLimit);
+        setUpperLimit(upperLimit);
+    }
+
+    public void evaluate(DoubleSolution solution) {
+        double[] x = new double[getNumberOfVariables()];
+        double fx = 0;
+
+        for (int i = 0; i < solution.getNumberOfVariables(); i++) {
+            x[i] = solution.getVariableValue(i);
+        }
+
+        for (int var = 0; var < solution.getNumberOfVariables(); var++) {
+            fx += x[var] * x[var];
+        }
+
+        solution.setObjective(0, -1.0 * fx);
+    }
+
+    public DoubleSolution createSolution() {
+        return new DefaultDoubleSolution(this);
+    }
+
+}
